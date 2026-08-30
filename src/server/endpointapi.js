7a882b49bbe648f3c9ce9c436c0a9779b6f105c9
أبو المجد <joouud200@gmail.com>
2026-08-30 18:04:05 +0300

    added products and reviews hooks

--- src/server/endpointapi.js
+++ src/server/endpointapi.js
@@ -77,3 +77,50 @@ export const deleteUser=async (id)=>{
   const response=await apiClient.delete(`/users/${id}`)
   return response.data
 }
+
+// Products
+export const getAllProducts=async()=>{
+  const response=await apiClient.get("/products")
+  return response.data
+}
+
+export const createProduct=async(payload)=>{
+  const response=await apiClient.post("/products",payload)
+  return response.data
+}
+
+export const searchProducts=async(query)=>{
+  const response=await apiClient.get(`/products/search?query=${query}`)
+  return response.data
+}
+
+export const getProductById=async(id)=>{
+  const response=await apiClient.get(`/products/${id}`)
+  return response.data
+}
+
+export const deleteProduct=async(id)=>{
+  const response=await apiClient.delete(`/products/${id}`)
+  return response.data
+}
+
+export const updateProduct=async(id,payload)=>{
+  const response=await apiClient.patch(`/products/update/${id}`,payload)
+  return response.data
+}
+
+// Reviews 
+export const getProductReviews=async(productId)=>{
+  const response=await apiClient.get(`/products/${productId}/reviews`)
+  return response.data
+}
+
+export const addReview=async(productId,payload)=>{
+  const response=await apiClient.post(`/products/${productId}/reviews`,payload)
+  return response.data
+}
+
+export const deleteReview=async(productId,reviewId)=>{
+  const response=await apiClient.delete(`/products/${productId}/reviews/${reviewId}`)
+  return response.data
+}
