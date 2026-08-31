25de8b4ed3970412e832a820455d30e3f796af67
yararoz123 <yrbwad2@gmail.com>
2026-08-31 17:52:37 +0300

    Edited import error and handel main.jsx to Toaster

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster  />
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
